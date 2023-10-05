540c4887dcfbdf33c62f0251337813f17e6749bf
im-sakino-1006 <114118270+im-sakino-1006@users.noreply.github.com>
2023-10-05 18:45:50 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-TEST=/opt/maven/apache-maven-2.2.1/bin/mvn
+TEST=/opt/maven/apache-maven-2.2.1/bin
 echo ${TEST}
-cd /opt/maven/apache-maven-2.2.1/bin/mvn
+cd /opt/maven/apache-maven-2.2.1/bin
 ls -la
 mvn.sh
