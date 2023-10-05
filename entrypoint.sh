f933027278f5782fb89802dd181f5cad2e35f965
im-sakino-1006 <114118270+im-sakino-1006@users.noreply.github.com>
2023-10-05 18:14:46 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,2 @@
-#!/bin/sh -l
-
-
-echo `find -name "apache-maven-2.2.1"`
-#mvn --version
-
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+#!/bin/sh
+exec /bin/sh /apache-maven-2.2.1/bin/maven.sh
